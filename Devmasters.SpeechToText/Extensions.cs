04e1d72c422eaf1af3aef5841b845588c6ab90b0
Michal <michal@michalblaha.cz>
2020-12-13 17:53:25 +0100

    extensions

---
+++ Devmasters.SpeechToText/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Devmasters.SpeechToText
+{
+    public static class Extensions
+    {
+
+        public static string ToText(this IEnumerable<Term> terms, bool withParagraphs = false)
+        {
+            var vtt = new VoiceToTextFormatter(terms);
+            return vtt.Text(withParagraphs);
+        }
+        public static List<VoiceToTextFormatter.TextWithTimestamp> ToTextWithTimestamps(this IEnumerable<Term> terms, TimeSpan timestampInterval, bool withParagraphs = false)
+        {
+            var vtt = new VoiceToTextFormatter(terms);
+            return vtt.TextWithTimestamps( timestampInterval, withParagraphs);
+        }
+    }
+}
